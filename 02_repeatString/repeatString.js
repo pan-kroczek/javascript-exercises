bb58392ceeca6211dcab16e18045becdb62dffc0
Matt <157317090+pan-kroczek@users.noreply.github.com>
2024-12-11 21:45:58 +0100

    Solution for the second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let phrase = '';
 
+    if (num < 0) {
+        phrase = 'ERROR';
+    }
+    else {
+        for (i = 0; i < num; i++) {
+            phrase += string;
+        }
+    }
+
+    return phrase;
 };
 
 // Do not edit below this line
