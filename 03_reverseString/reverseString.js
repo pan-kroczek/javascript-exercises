483c32f19f06c10ee0a780f72946da8ee445bb88
Matt <157317090+pan-kroczek@users.noreply.github.com>
2024-12-11 22:18:55 +0100

    Solution for the third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,21 @@
-const reverseString = function() {
+const reverseString = function(phrase) {
+    // transfer all characters to an array
+    let characters = [];
+    
+    for (i = 0; i < phrase.length; i++) {
+        characters[i] = phrase.charAt(i);
+    }
 
+    // create a new array with elements in reverse order
+    let reversedCharacters = characters.reverse();
+    let newPhrase = '';
+
+    // retrieve characters to form a final string
+    for (i = 0; i < reversedCharacters.length; i++) {
+        newPhrase += reversedCharacters[i];
+    }
+
+    return newPhrase;
 };
 
 // Do not edit below this line
