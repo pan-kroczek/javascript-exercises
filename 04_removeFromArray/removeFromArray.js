9e79e5d181c0fa55a8bc7a1b54f19a5cc0163c07
Matt <157317090+pan-kroczek@users.noreply.github.com>
2024-12-12 19:42:29 +0100

    Solution sufficient for the first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, value1, value2) {
+    let myArray = array;
+    let newArray = [];
+    
+    for (i = 0; i < myArray.length; i++) {
+        if (myArray[i] === value1) {
+            newArray = myArray.splice(i, 1);
+        }
+    }
+
+    return myArray;
 };
 
 // Do not edit below this line
