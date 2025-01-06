f418ce071ff692208084a9c2980bbb87c41a578f
Matt <157317090+pan-kroczek@users.noreply.github.com>
2025-01-06 15:55:54 +0100

    Solution for the ninth exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let original = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()
+    // get all letters from original string into an array
+    let array = Array.from(original);
+    // reverse the array
+    let reversed = array.toReversed().join('');
+    // compare the reversed string to the original one
+    return reversed === original ? true : false;
 };
 
 // Do not edit below this line
