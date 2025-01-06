d1feaf622d82a840e9060ca03a234c0058dfcdf0
Matt <157317090+pan-kroczek@users.noreply.github.com>
2025-01-06 13:43:56 +0100

    Solution for the eight exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, item) => {
+    return total + item;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, item) => {
+    return total * item;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0 || num === 1) {
+    return 1;
+  }
+
+  for (i = num - 1; i > 0; i--) {
+    num *= i;
+  }
+
+  return num;
 };
 
 // Do not edit below this line
