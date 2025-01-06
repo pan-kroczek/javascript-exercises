d0127a71a9d6f814e71312ccca441b60287969d2
Matt <157317090+pan-kroczek@users.noreply.github.com>
2025-01-06 16:47:58 +0100

    Solution for the tenth exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let value = Number(num);
+    let array = [1, 1];
+  
+    if (value === 0) {
+      return 0;
+    } else if (value < 0) {
+      return `OOPS`;
+    }
+    // create an array consisting of numbers up to num
+    for (i = 2; i <= value; i++) {
+      array[i] = array[i - 1] + array[i - 2];
+    }
+    // get the value of num's index
+    return array.at((value -1));
 };
 
 // Do not edit below this line
