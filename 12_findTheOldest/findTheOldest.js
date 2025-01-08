d516ed1d9adc09aff1393a711efb9c564bb26041
Matt <157317090+pan-kroczek@users.noreply.github.com>
2025-01-08 21:15:34 +0100

    Solution for the twelfth exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.forEach(person => {
+        let today = new Date();
+        let currentYear = today.getFullYear();
+        let age;
+      
+        if ("yearOfDeath" in person === false) {
+          age = currentYear - person.yearOfBirth;
+        } else {
+          age = person.yearOfDeath - person.yearOfBirth;
+        }
+      
+        return person.age = age;
+      })
+      
+      people.sort((first, second) => {
+        return second.age - first.age;
+      });
+      
+      return people[0];
 };
 
 // Do not edit below this line
